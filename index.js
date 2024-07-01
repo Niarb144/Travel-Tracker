13f887e8320ba0da27e5a6345bc0631ee9478531
Brian Teddy <teddybrian543@gmail.com>
2024-07-01 15:35:19 +0300

    Add country

--- index.js
+++ index.js
@@ -14,32 +14,30 @@ const db = new pg.Client({
 });
 
 db.connect();
-db.query("SELECT country_code FROM visited_countries", (err,res) => {
-  if(err){
-    console.error("Error executing query", err.stack);
-  }else{
-    country = res.rows;
-  }
-  db.end();
-});
-
-let country = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  function displayCountryCodes(array) {
-    const countries = array
-      .filter(item => item.hasOwnProperty('country_code'))
-      .map(item => item.country_code);
-  
-      res.render("index.ejs", {countries, total : countries.length});
-    console.log(countries);
-  }
-  displayCountryCodes(country);
-  
-  
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(result.rows);
+  res.render("index.ejs", {countries : countries, total : countries.length});
+  db.end();
+});
+
+app.post("/add", async (req, res) => {
+  let countryName = req.body.country;
+  console.log(countryName);
+
+  const check_country = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [countryName]);
+   console.log(check_country);
+   res.render("index.ejs");
+   db.end();
+  // const input = await db.query("INSERT INTO visited_countries(country_code) VALUES($A)", [input_country]);
 });
 
 app.listen(port, () => {
